5d25ce93b8fc383542a18ff61027f5d841ce9797
JingChen14870 <jingchen.consultant@gmail.com>
2016-05-16 13:07:42 -0700

    Add comment

--- MyDll/dllmain.cpp
+++ MyDll/dllmain.cpp
@@ -1,4 +1,4 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
+// dllmain.cpp : Defines the entry point for the DLL application. Comments
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
 					 )
